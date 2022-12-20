1fd3e7a5c6fec408b6631500a7408f81d8a67271
lazutkinkirill <lazutkin_k@inbox.ru>
2022-12-20 22:45:33 +0300

    finish task 6

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.WriteLine("введите число");
+        int a = int.Parse(Console.ReadLine());
+        if (a % 2 == 0)
+        {
+            Console.WriteLine("число чётное");
+        }
+        else
+        {
+            Console.WriteLine("число нечётное");
+        }
+    }
+}
